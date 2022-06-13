ae29c2eab6e33113332a840d8cb7f80b73d459b9
Keval Rakholiya <keval63674@gmail.com>
2022-06-13 17:22:50 +0530

    Update heroku urls

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@
 export default {
     MODE: 'local',
     SHOPIFY_API_KEY: 'SHOPIFY_API_KEY',
-    HOST: 'https://a51b-2405-201-2024-a0f6-387d-2940-a151-a5ed.ngrok.io'
+    HOST: 'https://shopify-node-react-app.herokuapp.com'
 }
